5df6742a0236f3ac82d2fb1eb80ae638078a7311
Mohit <mohitkyadav@outlook.com>
2017-06-15 13:49:06 +0530

    Added none_of [CPP]

---
+++ none_of/none_of.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v{ 2, 7, 1, 2, -2, 4, 0 };
+    bool result = none_of(begin(v), end(v), [](auto elem) {return elem > -4;});
+    cout << result << endl;
+    return 0;
+}
