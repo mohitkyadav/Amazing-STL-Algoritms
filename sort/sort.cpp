1432cebd68fd58af5992e7a4591291cd72f8c27e
Mohit <mohitkyadav@outlook.com>
2017-06-19 14:46:02 +0530

    Added sort

---
+++ sort/sort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v{ 4,3,2,0,15,2,100,-5,1000 };
+	sort(begin(v), end(v), [](int elem1, int elem2) {return elem1 > elem2;});
+	cout << v;
+}
