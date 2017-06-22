6477bc95cf78aada007c904efc4d82edf3b70e9c
Mohit <mohitkyadav@outlook.com>
2017-06-22 06:50:03 +0530

    Added partial_sort_copy

---
+++ partial_sort_copy/partial_sort_copy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print(vector<int> v)
+{
+    for(auto elem : v)
+    {
+        cout << elem << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+	vector<int> v{ 4,3,2,0,15,2,100,-5,1000 };
+	vector<int> v1(5);
+	partial_sort_copy(begin(v), end(v), begin(v1), end(v1));
+	print(v);
+	print(v1);
+	return 0;
+}
