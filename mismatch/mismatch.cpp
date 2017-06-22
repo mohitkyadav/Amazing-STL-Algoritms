9c00efa8b62f3049c71dfc92e8a427b025fd1de7
Mohit <mohitkyadav@outlook.com>
2017-06-22 15:04:54 +0530

    Added mismatch

---
+++ mismatch/mismatch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v1{ 2, 7, 1, 2, -2, 4, 0 };
+    vector<int> v2{ 2, 7, 2, 2, -2, 4, 0 };
+    auto diff = mismatch(begin(v1), end(v1), begin(v2));
+    cout << *diff.first << endl;
+    cout << *diff.second << endl;
+    return 0;
+}
