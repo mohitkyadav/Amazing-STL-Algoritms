d601e4497019aca82a17002eab85e6c80cbaadb7
Mohit <mohitkyadav@outlook.com>
2017-06-22 06:55:19 +0530

    Added nth_element

---
+++ nth_element/nth_element.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v{ 4,3,2,0,15,2,100,-5,1000 };
+	nth_element(begin(v), begin(v) + 6, end(v));
+	for(auto elem : v)
+    {
+        cout << elem << " ";
+    }
+	return 0;
+}
